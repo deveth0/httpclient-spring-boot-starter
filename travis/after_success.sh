f1678e694536cee4ec73ac0981e66aac93046e43
Alex Muthmann <amuthmann@dev-eth0.de>
2020-05-05 15:58:51 +0200

    Add after_success script to travis build

---
+++ travis/after_success.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Only deploy master and develop branch to sonatype
+if [ $TRAVIS_REPO_SLUG == "deveth0/httpclient-spring-boot-starter" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" || "$TRAVIS_BRANCH" == "develop" ] ; then
+
+  if [ $TRAVIS_JDK_VERSION == "openjdk11" ]; then
+
+    gpg2 --keyring=$TRAVIS_BUILD_DIR/pubring.gpg --no-default-keyring --import ./travis/signingkey.asc
+    gpg2 --allow-secret-key-import --keyring=$TRAVIS_BUILD_DIR/secring.gpg --no-default-keyring --import ./travis/signingkey.asc
+    echo -e "Successfully imported GPG keys"
+
+    mvn clean deploy --settings ./travis/maven-settings.xml -Dgpg.executable=gpg2 -Dgpg.keyname=D4E7E9033BBC3785DB28D8DBBE5F392A05E8EEC0 -Dgpg.passphrase=$PASSPHRASE -Dgpg.publicKeyring=$TRAVIS_BUILD_DIR/pubring.gpg -Dgpg.secretKeyring=$TRAVIS_BUILD_DIR/secring.gpg
+    echo -e "Successfully deployed to sonatype"
+
+  fi
+else
+  echo "Travis deployment skipped"
+fi
