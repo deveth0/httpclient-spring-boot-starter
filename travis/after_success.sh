c699a97dc1b03b82a24ed3665b54c859ce698712
Alex Muthmann <amuthmann@dev-eth0.de>
2020-05-06 12:33:16 +0200

    Update travis deployment

--- travis/after_success.sh
+++ travis/after_success.sh
@@ -5,9 +5,13 @@ if [[ $TRAVIS_REPO_SLUG == "deveth0/httpclient-spring-boot-starter" ]] && [[ "$T
   if [[ $TRAVIS_JDK_VERSION == "openjdk11" ]]; then
     gpg2 --allow-secret-key-import --keyring=$TRAVIS_BUILD_DIR/pubring.gpg --no-default-keyring --import ./travis/sonatype_signing.asc
     echo -e "Successfully imported GPG keys"
-    gpg2 --keyring=$TRAVIS_BUILD_DIR/pubring.gpg --list-secret-keys
 
-    mvn deploy -DskipTests=true -Prelease-profile --settings ./travis/maven-settings.xml -Dgpg.useAgent=false -Dgpg.executable=gpg2 -Dgpg.passphrase=$PASSPHRASE -Dgpg.publicKeyring=$TRAVIS_BUILD_DIR/pubring.gpg -Dgpg.secretKeyring=$TRAVIS_BUILD_DIR/pubring.gpg
+    echo - "Seckeys"
+    gpg2 --keyring=$TRAVIS_BUILD_DIR/pubring.gpg --list-secret-keys
+    echo -e "Pubkeys"
+    gpg2 --keyring=$TRAVIS_BUILD_DIR/pubring.gpg --list-public-keys
+
+    mvn deploy -DskipTests=true -Prelease-profile --settings ./travis/maven-settings.xml -Dgpg.useAgent=false -Dgpg.executable=gpg2 -Dgpg.keyname=05E8EEC0 -Dgpg.passphrase=$PASSPHRASE -Dgpg.publicKeyring=$TRAVIS_BUILD_DIR/pubring.gpg -Dgpg.secretKeyring=$TRAVIS_BUILD_DIR/pubring.gpg
     echo -e "Successfully deployed to sonatype"
 
   fi
