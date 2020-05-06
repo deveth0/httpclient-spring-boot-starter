96f69ceefc0211cc9bd56f4013b60be8df6ecd7b
Alex Muthmann <amuthmann@dev-eth0.de>
2020-05-06 13:36:16 +0200

    Update travis deployment

--- travis/after_success.sh
+++ travis/after_success.sh
@@ -3,7 +3,7 @@
 # Only deploy master and develop branch to sonatype
 if [[ $TRAVIS_REPO_SLUG == "deveth0/httpclient-spring-boot-starter" ]] && [[ "$TRAVIS_PULL_REQUEST" == "false" ]] && [[ "$TRAVIS_BRANCH" == "feature/travis_sonatype" || "$TRAVIS_BRANCH" == "develop" ]]; then
   if [[ $TRAVIS_JDK_VERSION == "openjdk11" ]]; then
-    gpg2 --allow-secret-key-import --keyring=$TRAVIS_BUILD_DIR/pubring.gpg --no-default-keyring --import ./travis/sonatype_signing.asc
+    gpg2 --allow-secret-key-import --batch --passphrase=$PASSPHRASE --keyring=$TRAVIS_BUILD_DIR/pubring.gpg --no-default-keyring --import ./travis/sonatype_signing.asc
     echo -e "Successfully imported GPG keys"
 
     mvn install deploy -DskipTests=true -Prelease-profile --settings ./travis/maven-settings.xml -Dgpg.executable=gpg2 -Dgpg.keyname=05E8EEC0 -Dgpg.passphrase=$PASSPHRASE -Dgpg.publicKeyring=$TRAVIS_BUILD_DIR/pubring.gpg -Dgpg.secretKeyring=$TRAVIS_BUILD_DIR/pubring.gpg
