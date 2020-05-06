6dcbcd81d2e8d000806301b4089db353c48e08ab
Alex Muthmann <amuthmann@dev-eth0.de>
2020-05-06 13:41:19 +0200

    Update travis deployment

--- travis/after_success.sh
+++ travis/after_success.sh
@@ -8,7 +8,6 @@ if [[ $TRAVIS_REPO_SLUG == "deveth0/httpclient-spring-boot-starter" ]] && [[ "$T
 
     mvn install deploy -DskipTests=true -Prelease-profile --settings ./travis/maven-settings.xml -Dgpg.executable=gpg2 -Dgpg.keyname=05E8EEC0 -Dgpg.passphrase=$PASSPHRASE -Dgpg.publicKeyring=$TRAVIS_BUILD_DIR/pubring.gpg -Dgpg.secretKeyring=$TRAVIS_BUILD_DIR/pubring.gpg
     echo -e "Successfully deployed to sonatype"
-
   fi
 else
   echo "Travis deployment skipped"
